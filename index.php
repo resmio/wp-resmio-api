2b144e6c7385f3d679968856707d6c1ec2d9d240
Renaldo Weiser <renaldo.weiser@gmx.de>
2016-04-22 13:51:41 +0200

    english/german translation fixes #19

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="de">
 <head profile="http://gmpg.org/xfn/11">
  
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/resmio-api/css/resmio-style.css">
+<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/resmio-api/css/resmio-style.css">
 <?php wp_head(); ?>
 
 </head>
@@ -12,10 +12,11 @@ transitional.dtd">
 <div id="wrapper">
 
 	<div id="header">
-	<img src="<?php bloginfo('stylesheet_directory'); ?>/resmio-api/img/resmio-logo.png"/>
+	<img src="<?php echo get_stylesheet_directory_uri(); ?>/resmio-api/img/resmio-logo.png"/>
 	</div><!-- header -->
 
 	<div id="main">
+		
 		<p><b><?php _e('Name', 'resmio_i18n'); ?>:</b><br><?php echo do_shortcode('[resmio-name]'); ?></p>
 		<p><b><?php _e('Adresse', 'resmio_i18n'); ?>:</b><br><?php echo do_shortcode('[resmio-address]'); ?></p>
 		<p><b><?php _e('Straße', 'resmio_i18n'); ?>:</b><br><?php echo do_shortcode('[resmio-street]'); ?></p>
@@ -25,8 +26,8 @@ transitional.dtd">
 		<p><b><?php _e('Telefon', 'resmio_i18n'); ?>:</b><br><?php echo do_shortcode('[resmio-phone]'); ?></p>
 		<p><b><?php _e('E-Mail', 'resmio_i18n'); ?>:</b><br><?php echo do_shortcode('[resmio-email]'); ?></p>
 		<p><b><?php _e('soziale Plattformen', 'resmio_i18n'); ?>:</b><br><?php echo do_shortcode('[resmio-social]'); ?></p>
-		<p><b><?php _e('Facebook (URL)', 'resmio_i18n'); ?>:</b><br><?php echo do_shortcode('[resmio-facebook]'); ?></p>
-		<p><b><?php _e('Google + (URL)', 'resmio_i18n'); ?>:</b><br><?php echo do_shortcode('[resmio-googleplus]'); ?></p>
+		<p><b><?php _e('Facebook', 'resmio_i18n'); ?>:</b><br><?php echo do_shortcode('[resmio-facebook]'); ?></p>
+		<p><b><?php _e('Google +', 'resmio_i18n'); ?>:</b><br><?php echo do_shortcode('[resmio-googleplus]'); ?></p>
 		<p><b><?php _e('Öffnungszeiten', 'resmio_i18n'); ?>:</b><br><?php echo do_shortcode('[resmio-openinghours]'); ?></p>
 		<p><b><?php _e('Beschreibung', 'resmio_i18n'); ?>:</b><br><?php echo do_shortcode('[resmio-description]'); ?></p>
 	
